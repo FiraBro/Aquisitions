a5bcd24103fe317f88b253b8f14d6838c0e83994
Firagos Jemal <jemalfiragos@gmail.com>
2025-12-17 00:40:45 +0300

    setup the logger

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,9 +1,9 @@
-import "dotenv/config.js";
+import 'dotenv/config.js';
 
 export default {
-  schema: "./src/models/*.js",
-  out: "./drizzle",
-  dialect: "postgresql",
+  schema: './src/models/*.js',
+  out: './drizzle',
+  dialect: 'postgresql',
   dbCredentials: {
     // FIXED: changed from dbCredential to dbCredentials
     url: process.env.DATABASE_URL,
--- src/app.js
+++ src/app.js
@@ -1,16 +1,38 @@
-import express from 'express';
-
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+import logger from "#config/logger.js";
 const app = express();
 
-// middleware
+// Determine environment
+const isProduction = process.env.NODE_ENV === "production";
+
+// Security middleware
+app.use(helmet());
+
+// CORS configuration
+app.use(
+  cors({
+    origin: isProduction
+      ? ["https://yourdomain.com"]
+      : ["http://localhost:3000"],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// health / root route
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Learning Kubernetes 🚀',
-    status: 'OK',
-  });
+app.use(
+  morgan(isProduction ? "combined" : "dev", {
+    stream: {
+      write: (message) => logger.info(message.trim()),
+    },
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.status(200).send("hello from app");
 });
-
 export default app;
