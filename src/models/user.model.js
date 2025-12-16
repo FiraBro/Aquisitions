badf1fbd562e43cc86fc238ceaa1b141b7ad373d
Firagos Jemal <jemalfiragos@gmail.com>
2025-12-16 23:17:29 +0300

    create th model for user

---
+++ src/models/user.model.js
@@ -0,0 +1,9 @@
+import { pgTable, serial, timestamp, varchar } from 'drizzle-orm/pg-core';
+export const user = pgTable('user', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 255 }).notNull(),
+  password: varchar('password', { length: 255 }).notNull(),
+  email: varchar('email', { length: 255 }).notNull().unique(),
+  createdAt: timestamp().defaultNow().notNull(),
+  updatedAT: timestamp().defaultNow().notNull(),
+});
